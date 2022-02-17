8bca4a6762fdad7b5ac187e0fbeb3540e0d9a688
bcbeltran <benjamin.beltran37@gmail.com>
2022-02-17 10:05:43 -0700

    Added blake2b using a key argument instead of data

--- blake2b.py
+++ blake2b.py
@@ -1,9 +1,17 @@
 from hashlib import blake2b
 
-data = b'Message for transmission'
+# data = b'Message for transmission'
 
-blake = blake2b(data, digest_size=32)
-print('Printing blake digest: ')
-print(blake.digest())
-print('Printing blake hexadecimal digest: ')
-print(blake.hexdigest())
+# blake = blake2b(data, digest_size=32)
+# print('Printing blake digest: ')
+# print(blake.digest())
+# print('Printing blake hexadecimal digest: ')
+# print(blake.hexdigest())
+
+blake = blake2b(key=b'keys', digest_size=60)
+blake.update(b'Blake message')
+blake_digest = blake.digest()
+blake_hexdigest = blake.hexdigest()
+
+print('Blake digest: ', blake_digest)
+print('Blake hexdigest: ', blake_hexdigest)
